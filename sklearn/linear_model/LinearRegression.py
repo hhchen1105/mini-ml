e272823919b38c68d488f10556d682e74c1ca773
Hung-Hsuan Chen <hhchen1105@gmail.com>
2024-07-23 19:13:14 +0800

    Update the place to import numpy

--- sklearn/linear_model/LinearRegression.py
+++ sklearn/linear_model/LinearRegression.py
@@ -1,6 +1,11 @@
-class sklearn.linear_model.LinearRegression(*, fit_intercept=True, copy_X=True, n_jobs=None, positive=False):
-    def __init__(self, *, fit_intercept=True, copy_X=True, n_jobs=None, positive=False):
+import numpy as np
+
+
+class LinearRegression():
+    def __init__(self, *, fit_intercept=True):
         self.fit_intercept = fit_intercept
-        self.copy_X = copy_X
-        self.n_jobs = n_jobs
-        self.positive = positive
+        self.coef_, self.intercept_, self.rank_, self.singular_ = None, None, None, None
+
+    def fit(self, X, y):
+        self.coef_, self.intercept_, self.rank_, self.singular_ = np.linalg.lstsq(X, y, rcond=None)
+        self.coef_ = self.coef_.T
