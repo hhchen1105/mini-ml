f96a5df91a0cbb88187bbe7d9e8c545183f9af3d
Hung-Hsuan Chen <hhchen1105@acm.org>
2024-09-28 14:55:11 +0800

    Remove unnecessary comments

--- miniml/sklearn/neighbors/KNeighborsClassifier.py
+++ miniml/sklearn/neighbors/KNeighborsClassifier.py
@@ -13,12 +13,8 @@ class KNeighborsClassifier:
         return np.array(predictions)
 
     def _predict(self, x):
-        # Compute distances between x and all examples in the training set
         distances = np.linalg.norm(self.X_train - x, axis=1)
-        # Sort by distance and return indices of the first k neighbors
         k_indices = np.argsort(distances)[:self.n_neighbors]
-        # Extract the labels of the k nearest neighbor training samples
         k_nearest_labels = self.y_train[k_indices]
-        # Return the most common class label
         most_common = np.bincount(k_nearest_labels).argmax()
         return most_common
