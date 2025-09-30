53e89b2f11f8f283ed18cc20e3954d342a557cf7
ching0725 <angela126578@gmail.com>
2025-09-30 16:49:30 +0800

    Revert "Implement SGDClassifier with fit/predict/predict_proba/test"
    This reverts commit f496531d399859ba67aebbcb6b5afb9e2b2349b4.

--- miniml/sklearn/linear_model/__init__.py
+++ miniml/sklearn/linear_model/__init__.py
@@ -1,6 +1,5 @@
 from .LinearRegression import LinearRegression
 from .SGDRegressor import SGDRegressor
-from .SGDClassifier import SGDClassifier
 from .Ridge import Ridge
 from .Lasso import Lasso
 from .ElasticNet import ElasticNet
@@ -9,7 +8,6 @@ from .LogisticRegression import LogisticRegression
 __all__ = [
     "LinearRegression",
     "SGDRegressor",
-    "SGDClassifier",
     "Ridge", 
     "Lasso", 
     "ElasticNet", 
