89709bf26cf9118c637e194232ecbc31c1565338
Shaliy0217 <127116267+Shaliy0217@users.noreply.github.com>
2025-11-02 16:23:45 +0800

    fix: __all__ declaration in neural_network module

--- miniml/sklearn/neural_network/__init__.py
+++ miniml/sklearn/neural_network/__init__.py
@@ -1,5 +1,5 @@
 from .BernoulliRBM import BernoulliRBM
 from .MLPRegressor import MLPRegressor
 
-all = ['BernoulliRBM',
-      'MLPRegressor']
+__all__ = ['BernoulliRBM',
+            'MLPRegressor']
